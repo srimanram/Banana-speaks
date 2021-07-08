f01e4b11385ef33ef2da653c6f410286b87467a4
srimanram <sriman1177@gmail.com>
2021-07-08 14:30:52 +0530

    fetching

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var btn = document.querySelector(".btn-trans");
 var textint=document.querySelector(".text-int");
 var output=document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function gettranslationurl(input){
+    return serverURL + "?" + "text=" +input
+}
+
+function errorhandler(error){
+    console.log("error occur",error);
+    alert("there is something wrong! try after sometime")
+}
 function click() {
-    output.innerText="banana "+ textint.value;
+   var inputText = textint.value;//taking input
+   fetch(gettranslationurl(inputText))
+   .then(response => response.json()) 
+   .then(json =>{
+       var translatedtext =json.contents.translated;
+       output.innerText = translatedtext; //outputing
+   })
+        .catch(errorhandler)
 };
 btn.addEventListener("click", click)
